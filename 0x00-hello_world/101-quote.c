6cd900dc64dc468d9b376b1feb270206cff2f9bd
David Elijah <stoneandrew417@gmail.com>
2023-07-13 15:54:53 -0700

    Hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stuio.h>
+#include<unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+        puts("\"and that piece of art is useful" - Dora Korpar, 2015-10-19
+");
+        return (1)
+}
+
